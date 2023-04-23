a34824dbe6004659885152e40a81759374ea5c66
Benedikt <mail@benedikt-schwering.de>
2023-04-23 20:15:41 +0200

    add logout function

---
+++ functions/logout/__init__.py
@@ -0,0 +1,44 @@
+import azure.functions as func
+from db import FAPDatabase
+from uuid import uuid4
+import json
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    with FAPDatabase('benutzer') as benutzer_collection:
+        body = req.get_json()
+
+        benutzer = benutzer_collection.find_one({
+            'loginName': body['loginName'],
+            'session_id': body['sitzung']
+        })
+
+        if benutzer:
+            benutzer_collection.update_one(
+                {
+                    '_id': benutzer['_id']
+                },
+                {
+                    '$unset': {
+                        'session_id': ''
+                    }
+                }
+            )
+
+            return func.HttpResponse(
+                body=json.dumps({
+                    'ergebnis': True
+                }),
+                status_code=200,
+                mimetype='applicatoin/json',
+                charset='utf-8'
+            )
+
+        return func.HttpResponse(
+            body=json.dumps({
+                'ergebnis': False
+            }),
+            status_code=200,
+            mimetype='applicatoin/json',
+            charset='utf-8'
+        )
