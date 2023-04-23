391573b61826e9a4886ca3e55eec4f462a60c5b6
Benedikt <mail@benedikt-schwering.de>
2023-04-23 20:01:39 +0200

    add check login name function

---
+++ functions/check_login_name/__init__.py
@@ -0,0 +1,31 @@
+import azure.functions as func
+from db import FAPDatabase
+import json
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    with FAPDatabase('benutzer') as benutzer_collection:
+        id = req.params.get('id')
+
+        if benutzer_collection.find_one({
+            'loginName': {
+                '$regex': f'^{id}'
+            }
+        }):
+            return func.HttpResponse(
+                body=json.dumps({
+                    'ergebnis': True
+                }),
+                status_code=200,
+                mimetype='applicatoin/json',
+                charset='utf-8'
+            )
+
+        return func.HttpResponse(
+            body=json.dumps({
+                'ergebnis': False
+            }),
+            status_code=200,
+            mimetype='applicatoin/json',
+            charset='utf-8'
+        )
